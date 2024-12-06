7b3b3ad60201bae23191d3b9dd0fcc52845f3ba7
Mahroyan <mahroyan.zoraiz@gmail.com>
2024-12-06 06:28:23 -0800

    first commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+print("I'll help you store yours and your friend birthdays, first tell me the names")
+you = input("Enter your name: ")
+friend1 = input("Enter your friend 1 name: ")
+friend2 = input("Enter your friend 2 name: ")
+
+print("Now tell me the bithdays")
+bday1 = input("Enter your birtday: ")
+bday2 = input("Enter friend 1 birthday: ")
+bday3 = input("Enter friend 2 birthday: ")
+
+print("\nOk so yours birthday is on", bday1)
+print(friend1, "birthday is on", bday2)
+print(friend2, "birthday is on", bday3)
+print("I'll make sure to remember them")
